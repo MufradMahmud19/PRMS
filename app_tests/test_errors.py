d5a57a81616185704362790e11a3703d1aca7a21
Mufrad Mahmud <66878837+MufradMahmud19@users.noreply.github.com>
2025-04-23 13:52:23 +0300

    Delete app_tests directory

--- app_tests/test_errors.py
+++
@@ -1,8 +0,0 @@
-def test_404_recovery_links(client):
-    response = client.get('/patients/999')
-    assert response.status_code == 404
-    assert 'hospital:search' in response.json['_links']
-    
-def test_400_validation_help(client):
-    response = client.post('/patients', json={"age": "invalid"})
-    assert 'hospital:validation-help' in response.json['_links']
