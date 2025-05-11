a2fc820c9ba6b772dd34460990b6981685bd90d6
Mufrad Mahmud <66878837+MufradMahmud19@users.noreply.github.com>
2025-05-11 20:37:29 +0300

    Delete run.py

--- run.py
+++
@@ -1,8 +0,0 @@
-from app import create_app
-
-app = create_app()
-
-if __name__ == '__main__':
-    print("Starting Flask application...")
-    print("Server will be available at http://127.0.0.1:5001")
-    app.run(host='127.0.0.1', port=5001, debug=True)
